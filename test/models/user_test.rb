b9b9710812ea874c874ec024dc176ad57737aee8
Harris Tang <harris.tang@gmail.com>
2013-10-02 22:20:47 -0700

    added user signup validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,44 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "a user should enter a first name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:first_name].empty?
+  end
+  
+  test "a user should enter a last name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:last_name].empty?
+  end
+  
+  test "a user should enter a profile name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+  end
+  
+  test "a user should have a unique profile name" do
+    user = User.new
+    user.first_name = "Jason"
+    user.last_name = "Seifer"
+    user.email = "jason@teamtreehouse.com"
+    user.profile_name = "jasonseifer"
+    user.password = "password"
+    user.password_confirmation = "password"
+
+    assert !user.save
+    puts user.errors.inspect
+    assert !user.errors[:profile_name].empty?
+  end
+
+  test "a user should have a profile name without spaces" do
+      user = User.new
+      user.profile_name = "My Profile With Spaces"
+
+      assert !user.save
+      assert !user.errors[:profile_name].empty?
+      assert user.errors[:profile_name].include?("Must be formatted correctly.") 
+  end
 end
